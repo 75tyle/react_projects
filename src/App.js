b23b838201737bc44b440cb8f94738d99e35ad0c
Sanghpal <kamblesanghpal2003@gmail.com>
2024-06-27 17:10:04 +0530

    asdfghjkl;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import Accordian from "./components/accordian";
 import ImageSlider from "./components/image-slider";
+import LightDarkMode from "./components/light-dark-mode";
 import LoadMoreData from "./components/load-more-data";
+import QRCodeGenerator from "./components/qr-code-generator";
 import RandomColor from "./components/random-color";
+import ScrollIndicator from "./components/scroll-indicator";
 import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
@@ -26,6 +31,18 @@ function App() {
 
       {/* Load more products component*/}
       <LoadMoreData />
+
+      {/* Tree view component / menu UI component / recursive navigation component  */}
+      <TreeView menus={menus}/>
+
+      {/* QR code genrator*/}
+      <QRCodeGenerator />
+
+      {/* Light Dark Theme Mode */}
+      <LightDarkMode />
+
+      {/* Scroll Indicator */}
+      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} />
     </div>
   );
 }
---
+++ src/components/scroll-indicator/index.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+export default function ScrollIndicator({ url }) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  async function fetchData(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+      const data = await response.json();
+
+      console.log(data);
+
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+      setErrorMessage(e.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
+
+  function handleScrollPercentage() {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  console.log(data, loading);
+  return (
+    <div>
+      <h1>Custom Scroll Indicator</h1>
+      <div className="data-container">
+        {data && data.length > 0
+          ? data.map((dataItem) => <p>{dataItem.title}</p>)
+          : null}
+      </div>
+    </div>
+  );
+}
